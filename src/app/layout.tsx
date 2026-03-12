d44789156c233b8ddc4064c21b7216262c8eb5c6
ecoh04 <elad@wideopenplatform.com>
2026-03-12 17:55:06 +0200

    design revamp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="min-h-screen flex flex-col bg-white text-stone-900 antialiased">
+      <body className="min-h-screen flex flex-col bg-stone-950 text-stone-50 antialiased">
         <RootLayoutClient>{children}</RootLayoutClient>
       </body>
     </html>
